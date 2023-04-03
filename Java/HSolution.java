140d0027afd6a451a235cb7a9f7cea7b1786e4e4
Trushita <v8806044115@gmail.com>
2023-04-03 15:50:12 +0530

    my first day of coding

---
+++ Java/HSolution.java
@@ -0,0 +1,22 @@
+// Author: Trushita
+// Source: HackerRank
+// Task: In this challenge, you must read 3 integers from stdin and then print them to stdout. Each integer must be printed on a new line. To make the problem a little easier, a portion of the code is provided for you in the editor below.
+
+import java.io.*;
+import java.util.*;
+
+public class HSolution {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+    }
+}
